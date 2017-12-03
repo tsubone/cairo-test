a26399d77a46fdd5c2ca6f079ca7fc4cfc2400aa
Takashi Tsubone <takashi@hilbert>
2017-12-03 16:22:31 +0900

    update.

--- cairo-example.c
+++ cairo-example.c
@@ -5,18 +5,125 @@
 #include <cairo-xlib.h>
 #include <X11/Xlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static cairo_surface_t *bg_img;
+static cairo_surface_t *meter_img;
+static cairo_surface_t *speed_needle_img;
+static cairo_surface_t *tacho_needle_img;
+
+static cairo_t *speed_needle_ctx;
+static cairo_t *tacho_needle_ctx;
+
+static char *speed_needle_data;
+static char *tacho_needle_data;
+
+#define NEEDLE_WIDTH  300
+#define NEEDLE_HEIGHT 300
+
+static int
+my_cairo_initialize (cairo_t *cairo)
+{
+  bg_img    = cairo_image_surface_create_from_png("meter.png");
+  meter_img = cairo_image_surface_create_from_png("meter.png");
+
+  speed_needle_data = malloc (NEEDLE_WIDTH * NEEDLE_HEIGHT * 4);
+
+  speed_needle_img = cairo_image_surface_create_for_data (speed_needle_data,
+							  CAIRO_FORMAT_ARGB32,
+							  NEEDLE_WIDTH,
+							  NEEDLE_HEIGHT,
+							  NEEDLE_WIDTH * 4);
+  speed_needle_ctx = cairo_create (speed_needle_img);
+
+  tacho_needle_data = malloc (NEEDLE_WIDTH * NEEDLE_HEIGHT * 4);
+  tacho_needle_img = cairo_image_surface_create_for_data (tacho_needle_data,
+							  CAIRO_FORMAT_ARGB32,
+							  NEEDLE_WIDTH,
+							  NEEDLE_HEIGHT,
+							  NEEDLE_WIDTH * 4);
+  tacho_needle_ctx = cairo_create (tacho_needle_img);
+
+  //  cairo_surface_destroy (meter_img);
+
+  return 0;
+}
+
+static int my_count = 0;
+
+static int
+draw_speed_needle(cairo_t *ctx, int count)
+{
+  cairo_set_source_rgba (ctx, 1.0, 0, 0, 0.5);
+  cairo_paint (ctx);
+  
+  cairo_set_line_width(ctx, 1);
+  cairo_set_source_rgb(ctx, 1.0, 1.0, 0);
+  cairo_rectangle(ctx, 20+count%450, 20+count%350, 50, 50);
+  cairo_stroke(ctx);
+
+  return 0;
+}
+
+static int
+draw_tacho_needle(cairo_t *ctx, int count)
+{
+  cairo_set_source_rgba (ctx, 0, 0, 1.0, 0.5);
+  cairo_paint (ctx);
+  
+  cairo_set_line_width(ctx, 1);
+  cairo_set_source_rgb(ctx, 1.0, 1.0, 0);
+  cairo_rectangle(ctx, 20+count%450, 20+count%350, 50, 50);
+  cairo_stroke(ctx);
+
+  return 0;
+}
+
+static int
+my_cairo_paint (cairo_t *cairo) {
+
+  fprintf(stderr, "paint\n");
+
+  my_count++;
+
+  draw_speed_needle(speed_needle_ctx, my_count);
+
+  draw_tacho_needle(tacho_needle_ctx, my_count);
+
+  return 0;
+}
+
+#define SPEED_POS_X 30.0
+#define SPEED_POS_Y 30.0
+
+#define TACHO_POS_X 450.0
+#define TACHO_POS_Y 30.0
+
+static int
+my_composite (cairo_t *cairo) {
+  fprintf(stderr, "composite \n");
+
+  cairo_set_operator(cairo, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_surface(cairo, bg_img, 0, 0);
+  cairo_paint(cairo);
+
+  cairo_set_operator(cairo, CAIRO_OPERATOR_OVER);
+  cairo_set_source_surface(cairo, meter_img, 0, 0);
+  cairo_paint(cairo);
+
+  cairo_set_source_surface(cairo, speed_needle_img, SPEED_POS_X, SPEED_POS_Y);
+  cairo_paint(cairo);
+
+  cairo_set_source_surface(cairo, tacho_needle_img, TACHO_POS_X, TACHO_POS_Y);
+  cairo_paint(cairo);
+
+  return 0;
+}
 
 static Display *dpy;
 static Window win;
-
-static void
-my_paint_to_cairo (cairo_t *cairo) {
-  fprintf(stderr, "paint\n");
-  cairo_set_line_width(cairo, 1);
-  cairo_set_source_rgb(cairo, 128, 128, 0);
-  cairo_rectangle(cairo, 20, 20, 50, 50);
-  cairo_stroke(cairo);
-}
 
 static cairo_surface_t*
 my_create_cairo_surface (int w, int h)
@@ -52,11 +159,14 @@ my_event_loop (cairo_t *cairo)
       case MapNotify:
       case Expose:
       case ConfigureNotify:
-        my_paint_to_cairo (cairo);
+        my_cairo_paint (cairo);
+	my_composite (cairo);
+
 	if (e.type == Expose)
 	  {
 	    //	    fprintf (stderr, "send expose %d\n", (int)win);
 	    XSendEvent(dpy, win, False, NoEventMask, &e);
+	    usleep (16666); /* 16msec */
 	  }
         break;
     }
@@ -69,8 +179,9 @@ int main() {
   cairo_t *cairo;
   cairo_surface_t *surface;
 
-  surface = my_create_cairo_surface (200, 200);
+  surface = my_create_cairo_surface (750, 400);
   cairo = cairo_create(surface);
+  my_cairo_initialize (cairo);
 
   my_event_loop (cairo);
 
